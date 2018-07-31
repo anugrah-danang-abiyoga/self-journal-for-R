0806af65803aebdfd0e54301261a1b90ac30bfb6
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-07-31 21:03:48 +1200

    added first chapter of learning

--- firstScript.R
+++ firstScript.R
@@ -1,11 +1,11 @@
-# First assignment
+# First assignment: add numbers
 # Calculate 3 + 4
 3 + 4
 
 # Calculate 6 + 12
 6 + 12
 
-# Second assignment
+# Second assignment: use symbols to calculate numbers
 # An addition
 5 + 5 
 
@@ -23,3 +23,36 @@
 
 # Modulo
 28 %% 6
+
+#Third assignment: assigning number
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+#Fourth assignment: assigning number and add
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- 6 
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+my_fruit
+
+# Fifth assignment: use of string, numeric and statement
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
