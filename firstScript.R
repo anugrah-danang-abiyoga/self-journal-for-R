612d4cb573f6d37e5ab09c53704a4147f49732dc
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-07-31 20:49:15 +1200

    second commit for second assignment

--- firstScript.R
+++ firstScript.R
@@ -1,5 +1,25 @@
+# First assignment
 # Calculate 3 + 4
 3 + 4
 
 # Calculate 6 + 12
 6 + 12
+
+# Second assignment
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
