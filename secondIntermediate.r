af1b12f6affa050e4a42dc42b3ffb0cc7364d96c
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-20 18:15:57 +1200

    up to last question

---
+++ secondIntermediate.r
@@ -0,0 +1,131 @@
+# 1st
+# Initialize the speed variable
+speed <- 64
+
+# Code the while loop
+while (speed > 30) {
+  print("Slow down!")
+  speed <- speed - 7
+}
+
+# Print out the speed variable
+speed
+
+# 2nd
+# Initialize the speed variable
+speed <- 64
+
+# Extend/adapt the while loop
+while (speed > 30) {
+  print(paste("Your speed is",speed))
+  if (speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  } else {
+    print("Slow down!")
+    speed <- speed -6
+  }
+}
+
+#3rd 
+# Initialize the speed variable
+speed <- 88
+
+while (speed > 30) {
+  print(paste("Your speed is", speed))
+  
+  # Break the while loop when speed exceeds 80
+  if (speed > 80) {
+    break
+  }
+  
+  if (speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  } else {
+    print("Slow down!")
+    speed <- speed - 6
+  }
+}
+
+# 4th
+# Initialize i as 1 
+i <- 1
+
+# Code the while loop
+while (i <= 10) {
+  print(3 * i)
+  if ((i * 3) %% 8 == 0) {
+    break
+  }
+  i <- i + 1
+}
+
+# 5th 
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for (d in linkedin) {
+  print(d)
+}
+
+# Loop version 2
+for (d in 1:length(linkedin)) {
+  print(linkedin[d])
+}
+
+# 6th
+# The nyc list is already specified
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+
+# Loop version 1
+for (d in nyc) {
+  print(d)
+}
+
+# Loop version 2
+for(d in 1:length(nyc)) {
+  print(nyc[[d]])
+}
+
+# 7th 
+# The tic-tac-toe matrix ttt has already been defined for you
+
+# define the double for loop
+for (i in 1:nrow(ttt)) {
+  paste(ttt)
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row", i, "and column", j, "the board contains", ttt[i,j]))
+  }
+}
+
+# 8th 
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Code the for loop with conditionals
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  print(li)
+}
+
+# 9th 
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Code the for loop with conditionals
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  print(li)
+}
