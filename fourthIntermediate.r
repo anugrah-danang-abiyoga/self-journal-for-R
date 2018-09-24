9094c9252a6922776b73ecc3a5619a49f17ef608
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-24 22:14:10 +1200

    half-way of the module

---
+++ fourthIntermediate.r
@@ -0,0 +1,68 @@
+# 1st 
+# The vector pioneers has already been created for you
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+
+# Split names from birth year
+split_math <- strsplit(pioneers, split = ":")
+
+# Convert to lowercase strings: split_low
+# lapply(tolower(split_math, "FUN"))
+split_low <- lapply(split_math, tolower)
+
+# Take a look at the structure of split_low
+str(split_low)
+
+# 2nd 
+# Code from previous exercise:
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+split <- strsplit(pioneers, split = ":")
+split_low <- lapply(split, tolower)
+
+# Write function select_first()
+select_first <- function(x) {
+  x[1]
+}
+
+# Apply select_first() over split_low: names
+names <- lapply(split_low, select_first)
+names
+# Write function select_second()
+select_second <- function(x) {
+  x[2]
+}
+
+# Apply select_second() over split_low: years
+years <- lapply(split_low, select_second) 
+years
+
+# 3rd: The other way
+# Definition of split_low
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+split <- strsplit(pioneers, split = ":")
+split_low <- lapply(split, tolower)
+
+# Transform: use anonymous function inside lapply
+names <- lapply(split_low, function(x) {x[1]})
+names
+# Transform: use anonymous function inside lapply
+years <- lapply(split_low, function(x) {x[2]})
+years
+
+# 4th: Another way with selectal()
+# Definition of split_low
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+split <- strsplit(pioneers, split = ":")
+split_low <- lapply(split, tolower)
+
+# Generic select function
+select_el <- function(x, index) {
+  x[index]
+}
+
+# Use lapply() twice on split_low: names and years
+names <- lapply(split_low, select_el, index = 1)
+names
+years <- lapply(split_low, select_el, index = 2)
+years
+
+## The use of sapply() will return vector
