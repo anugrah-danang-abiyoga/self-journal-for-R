be87a3c73a5626889f04228bcd8d5cf2b3e27435
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2019-02-06 18:21:34 +1300

    Did some new learnings on readr package

--- importingData.R
+++ importingData.R
@@ -46,3 +46,21 @@ hotdogs2 <- read.delim("hotdogs.txt", header = FALSE,
 
 # Display structure of hotdogs2
 str(hotdogs2)
+
+## Load the readr package
+library(readr)
+
+# Import potatoes.csv with read_csv(): potatoes
+potatoes <- read_csv("potatoes.csv")
+
+# readr is already loaded
+
+# Column names
+properties <- c("area", "temp", "size", "storage", "method",
+                "texture", "flavor", "moistness")
+
+# Import potatoes.txt: potatoes
+potatoes <- read_tsv("potatoes.txt", col_names = properties)
+
+# Call head() on potatoes
+head(potatoes)
