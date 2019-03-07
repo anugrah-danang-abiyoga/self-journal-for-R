411ca4cb841c385669b4fd60055fed0bfd2a8cda
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2019-03-07 14:39:41 +1300

    1 chapter left

--- ImportingDataFromHTTP.R
+++ ImportingDataFromHTTP.R
@@ -80,3 +80,85 @@ content(resp, as = "text")
 
 # Print content of resp
 content(resp)
+
+## API & JSON
+
+# 1st: Load and see the structure of the JSON file using the R sytax
+# Load the jsonlite package
+library(jsonlite)
+
+# wine_json is a JSON
+wine_json <- '{"name":"Chateau Migraine", "year":1997, "alcohol_pct":12.4, "color":"red", "awarded":false}'
+
+# Convert wine_json into a list: wine
+wine <- fromJSON(wine_json)
+
+# Print structure of wine
+str(wine)
+
+# 2nd: render the structure of the data from the given API
+# Definition of quandl_url
+quandl_url <- "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?auth_token=i83asDsiWUUyfoypkgMz"
+
+# Import Quandl data: quandl_data
+quandl_data <- fromJSON(quandl_url)
+
+# Print structure of quandl_data
+str(quandl_data)
+
+# 3rd: add some expression to compare the year
+# The package jsonlite is already loaded
+
+# Definition of the URLs
+url_sw4 <- "http://www.omdbapi.com/?apikey=72bc447a&i=tt0076759&r=json"
+url_sw3 <- "http://www.omdbapi.com/?apikey=72bc447a&i=tt0121766&r=json"
+
+# Import two URLs with fromJSON(): sw4 and sw3
+sw4 <- fromJSON(url_sw4)
+sw3 <- fromJSON(url_sw3)
+
+# Print out the Title element of both lists
+sw4$Title
+sw3$Title
+
+# Is the release year of sw4 later than sw3?
+sw4$Year > sw3$Year
+
+# 4th: Use fromJSON() to play around with JSON array & object
+# jsonlite is already loaded
+
+# Challenge 1
+json1 <- fromJSON('[1, 2, 3, 4, 5, 6]')
+json1
+
+# Challenge 2
+json2 <- fromJSON('{"a": [1, 2, 3], "b": [4, 5, 6]}')
+json2
+
+# 5th: make the file into JSON by using toJSON()
+# URL pointing to the .csv file
+url_csv <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/water.csv"
+
+# Import the .csv file located at url_csv
+water <- read.csv(url_csv, stringsAsFactors = FALSE)
+
+# Convert the data file according to the requirements
+water_json <- toJSON(water)
+
+# Print out water_json
+water_json
+
+# 6th: practice minify and make the JSON obj neat
+# jsonlite is already loaded
+
+# Convert mtcars to a pretty JSON: pretty_json
+pretty_json <- toJSON(mtcars, pretty = TRUE)
+
+# Print pretty_json
+pretty_json
+
+# Minify pretty_json: mini_json
+mini_json <- minify(pretty_json)
+
+# Print mini_json
+mini_json
